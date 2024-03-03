8e898fe9a6ea04777ad490670e23b0b22cfbae93
Weixin Yu <weixinyu@caen-vnc-mi17.engin.umich.edu>
2024-03-02 22:45:42 -0500

    add test_integrate

--- test/rs_test.sv
+++ test/rs_test.sv
@@ -433,6 +433,55 @@ module testbench;
 
         $display("@@@ Passed: test_execute");
     endtask
+    
+    task test_integrate;
+        begin
+            init;
+            for (int j = 0; j < `RS_SZ/`N; ++j) begin
+                for (int i = 0; i < `N; ++i) begin
+                    rs_is_packet.entries[i] = '{
+                        $random,  // unused
+                        `TRUE, // valid
+                        $random, // PC
+                        FU_ALU,
+                        ALU_ADD,
+                        `FALSE, // op1_ready
+                        `TRUE, // op2_ready
+                        32'h1,  // op1
+                        $random,  // op2
+                        32'h3,  // dest_prn
+                        {`ROB_CNT_WIDTH{1'h0}} // robn
+                    };
+                end
+                @(negedge clock);
+            end
+
+            failed = (counter_out != `RS_SZ);
+            for (int i = 0; i < `NUM_FU_ALU; ++i) begin
+                fu_alu_avail[i] = `TRUE;
+            end
+            @(negedge clock);
+
+            for (int i = 0; i <`N ; ++i) begin
+                rs_is_packet.entries[i].valid = `FALSE;
+            end
+
+            failed = (counter_out != `RS_SZ);
+
+            cdb_packet[0] = '{
+                `TRUE,
+                32'h1,
+                $random
+            };
+            for (int i = 0; i < `RS_SZ/`N; i++) begin
+                @(negedge clock);
+                cdb_packet[0].valid = `FALSE;
+                failed = (counter_out != `RS_SZ - min(`N, `NUM_FU_ALU) * i);
+            end
+            @(negedge clock);
+            $display("@@@ Passed: test_integrate");
+        end
+    endtask
 
     
 
@@ -468,9 +517,10 @@ module testbench;
             test_random_cdb;
         end
         test_dummy_execute;
-        for (int i = 0; i < 1000; ++i) begin
+        for (int i = 0; i < 100; ++i) begin
             test_execute;
         end
+        test_integrate;
         
         $finish;
     end
