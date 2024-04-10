72930679d1c78b9594a7624f6ff3ed10441346e7
Weixin <weixinyu@umich.edu>
2024-04-09 20:10:38 -0400

    store queue impl

--- verilog/store_queue.sv
+++ verilog/store_queue.sv
@@ -52,6 +52,7 @@ module store_queue (
     // LQ
     input  ADDR  [`NUM_FU_LOAD-1:0] addr,
     input  logic [`NUM_FU_LOAD-1:0][`SQ_IDX_BITS-1:0] store_range,
+    input MEM_SIZE [`NUM_FU_LOAD-1:0] load_byte_info,
     output DATA  [`NUM_FU_LOAD-1:0] value,
     output logic [`NUM_FU_LOAD-1:0] fwd_valid
 `ifdef CPU_DEBUG_OUT
@@ -69,11 +70,13 @@ module store_queue (
     SQ_ENTRY[(`SQ_LEN+1)-1:0] entries, next_entries;
 
     logic[`SQ_IDX_BITS-1:0] size, next_size, next_head, next_tail, next_tail_ready;
+    logic[`SQ_IDX_BITS-1:0] idx;
     SQ_REG[`NUM_FU_STORE-1:0] sq_reg, next_sq_reg;
 
     assign almost_full = size > SQ_LEN - `NUM_FU_LOAD;
 
     always_comb begin
+        next_sq_reg = sq_reg;
         foreach (next_sq_reg[i]) begin
             next_sq_reg[i].valid  = rs_sq_packet[i].valid;
             next_sq_reg[i].addr   = rs_sq_packet[i].addr + {20'h0, rs_sq_packet[i].offset};
@@ -82,6 +85,8 @@ module store_queue (
         end
     end
 
+    // entry
+    logic[`SQ_IDX_BITS-1:0] idx;
     always_comb begin
         next_entries = entries;
 
@@ -97,9 +102,7 @@ module store_queue (
                     entries[tail] = '{
                         `TRUE,
                         id_sq_packet[i].byte_info,
-                        0,
-                        0,
-                        `FALSE
+                        0, 0, `FALSE
                     };
                     next_tail = (next_tail + 1) % (`SQ_LEN + 1);
                     next_size += 1;
@@ -116,24 +119,76 @@ module store_queue (
         // ROB
         sq_dcache_packet = 0;
         num_sent_insns = 0;
+        idx = 0;
         for (int i = 0; i < `NUM_SQ_DCACHE; i++) begin
-            if (num_commit_insns - num_sent_insns > 0) begin
-                if (entries[head+i].valid && entries[head+i].ready) begin
+            if (num_commit_insns > num_sent_insns) begin
+                idx = (head + i) % (`SQ_LEN + 1);
+                if (entries[idx].valid && entries[idx].ready) begin
                     sq_dcache_packet = '{
                         `TRUE,
-                        entries[head+i].addr,
-                        entries[head+i].byte_info,
-                        entries[head+i].data
+                        entries[idx].addr,
+                        entries[idx].byte_info,
+                        entries[idx].data
                     };
                     if (dcache_accept[i]) begin
                         num_sent_insns += 1;
-                        entries[head+i] = 0;
+                        next_entries[idx] = 0;
                     end
                 end
             end
         end
         next_head = (head + num_sent_insns) % (`SQ_LEN + 1);
+    end
 
+    // tail_ready
+    logic[`SQ_IDX_BITS-1:0] idx_tail;
+    logic flag_ready;
+    always_comb begin
+        tail_ready = head;
+        flag_ready = `TRUE;
+        for (int i = 0; i < `SQ_LEN; i++) begin
+            idx_tail = (head + i) % (`SQ_LEN + 1);
+            if (flag_ready && entries[idx_tail].valid) begin
+                flag_ready &= entries[idx_tail].ready;
+                if (entries[idx_tail].ready) begin
+                    tail_ready = (tail_ready + 1) % (`SQ_LEN + 1);
+                end
+            end
+        end
+    end
+
+    // LQ fwd
+    logic[`SQ_IDX_BITS-1:0] idx_fwd;
+    logic flag_break;
+    logic match, match_byte, match_half, match_word, match_dble, compatible;
+    always_comb begin
+        flag_matched = `FALSE;
+        for (int i = 0; i < `NUM_FU_LOAD; i++) begin
+            value[i] = 32'h0;
+            fwd_valid[i] = `FALSE;
+            for (int j = 0; j < `SQ_LEN; j++) begin
+                idx_fwd = (head + j) % (`SQ_LEN + 1);
+
+                match_byte = entries[idx_fwd].addr[31:0] == addr[i][31:0];
+                match_half = entries[idx_fwd].addr[31:1] == addr[i][31:1];
+                match_word = entries[idx_fwd].addr[31:2] == addr[i][31:2];
+                match_dble = entries[idx_fwd].addr[31:3] == addr[i][31:3];
+                compatible = entries[idx_fwd].byte_info >= load_byte_info[i];
+
+                case (entries[idx_fwd].byte_info)
+                    BYTE:   match = match_byte && compatible;
+                    HALF:   match = match_half && compatible;
+                    WORD:   match = match_word && compatible;
+                    DOUBLE: match = match_dble && compatible;
+                endcase
+
+                flag_break |= idx_fwd == store_range[i];
+                if (~flag_break && match) begin
+                    value[i] = entries[idx_fwd].data;
+                    fwd_valid[i] = `TRUE;
+                end
+            end
+        end
     end
 
     always_ff @(posedge clock) begin
@@ -143,19 +198,17 @@ module store_queue (
             // end
             entries <= $bits(entries)'h0;
 
-            size <= next_size;
-            head <= next_head;
-            tail <= next_tail;
-            // tail_ready <= next_tail_ready;
+            size <= `SQ_IDX_BITS'h0;
+            head <= `SQ_IDX_BITS'h0;
+            tail <= `SQ_IDX_BITS'h0;
 
             sq_reg <= $bits(sq_reg)'h0;
         end else begin
             entries <= next_entries;
 
-            size <= `SQ_IDX_BITS'h0;
-            head <= `SQ_IDX_BITS'h0;
-            tail <= `SQ_IDX_BITS'h0;
-            // tail_ready <= `SQ_IDX_BITS'h0;
+            size <= next_size;
+            head <= next_head;
+            tail <= next_tail;
 
             sq_reg <= next_sq_reg;
         end
